2204a8dcb901e0bf6b35f591b47237cc302602c4
Vitor Souza <vitorugo_kta@hotmail.com>
2022-10-30 08:31:20 -0300

    Adicionando o primeiro exercicio na pasta codificação back end

---
+++ codificação_back_end/Exercícios/exercício_1/ex.js
@@ -0,0 +1,58 @@
+/*  Exercicio 1 
+
+// ""
+console.log("Primeira aula")
+
+// '' 
+console.log('primeira aula')
+
+// ``
+num = 1
+console.log(`${num}° aula`)
+
+// Concatenando 
+console.log('1' + `${2}`)
+ */ 
+
+/* Exercicio 2 *
+
+// inteiro
+console.log(`Numero inteiro: ${2}`)
+
+// decimal 
+console.log(`Numero decimal: ${2.987}`)
+
+// aritmética
+console.log(`Operação aritmética: ${(2+3) * 2}`) /
+
+/* Exercicio 3 
+
+// maior
+console.log(3 > 4)
+
+//menor
+console.log(2 < 3)
+
+// igual
+console.log(1 == 2)
+
+// diferente
+console.log(1 != 3) */
+
+/* Exercicio 4 
+const a = 3;
+const b = -2;
+const c = 2;
+const d = 2;
+
+console.log(!(a > 0 || b > 0));
+console.log((c && d) == 2);
+console.log((a || c) == 2); */
+
+/* Exercicio 5 
+// NaN
+console.log('a' * 2); */ 
+
+
+
+
